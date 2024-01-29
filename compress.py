c3a11de3b342209ba53421ad2d41cefa44115bbc
devgeekm <118623500+devgeekm@users.noreply.github.com>
2024-01-29 15:14:05 -0500

    remove check directory invalid

--- compress.py
+++ compress.py
@@ -16,22 +16,18 @@ def comprimir_video(ruta_origen, ruta_destino):
     # Ejecuta el comando
     subprocess.run(comando, check=True)
 
-print("¡Bienvenido a Compress HandBrakeCLI, su archivo se guardara en el mismo directorio que el archivo de origen!")
+print("¡Bienvenido a Compress MP4, su archivo se guardara en el mismo directorio que el archivo de origen!")
 # Solicita la ruta del archivo de origen convirtiendo la ruta en relativa para que acepte espacios en el nombre del archivo o en la ruta del directorio que lo contiene
 ruta_origen = input("Ingrese la ruta del archivo de origen: ").strip()
 
-# Comprueba si la ruta es válida
-if not os.path.isfile(ruta_origen):
-    print("La ruta ingresada no es válida.")
-else:
-    # Obtiene la ruta del directorio del archivo de origen
-    ruta_directorio = os.path.dirname(ruta_origen)
+# Obtiene la ruta del directorio del archivo de origen
+ruta_directorio = os.path.dirname(ruta_origen)
 
-    # Crea la ruta del archivo de destino en el mismo directorio que el archivo de origen
-    nombre_archivo = os.path.basename(ruta_origen)
-    nombre_base, extension = os.path.splitext(nombre_archivo)
-    nombre_archivo_comprimido = f"{nombre_base}_comprimido{extension}"
-    ruta_destino = os.path.join(ruta_directorio, nombre_archivo_comprimido)
+# Crea la ruta del archivo de destino en el mismo directorio que el archivo de origen
+nombre_archivo = os.path.basename(ruta_origen)
+nombre_base, extension = os.path.splitext(nombre_archivo)
+nombre_archivo_comprimido = f"{nombre_base}_comprimido{extension}"
+ruta_destino = os.path.join(ruta_directorio, nombre_archivo_comprimido)
 
-    # Uso de la función
-    comprimir_video(ruta_origen, ruta_destino)
+# Uso de la función
+comprimir_video(ruta_origen, ruta_destino)
